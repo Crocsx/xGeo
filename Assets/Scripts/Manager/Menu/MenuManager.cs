7a4c230fb31d5a9dd780d8ff8d8f1f654f664797
Crocsx <Crocsx.gaming@gmail.com>
2018-01-16 00:30:31 +0900

    Very Basic Controller Manager

---
+++ Assets/Scripts/Manager/Menu/MenuManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class PanelInfo
+{
+    public string name;
+    public GameObject panel;
+}
+
+public class MenuManager : MonoBehaviour {
+
+    [Header("Panels")]
+    public PanelInfo[] panelInfoList;
+
+
+    Dictionary<string, GameObject> panels = new Dictionary<string, GameObject>();
+
+
+    void Start () {
+		for(int i=0; i < panelInfoList.Length; i++)
+        {
+            panels.Add(panelInfoList[i].name, panelInfoList[i].panel);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GoTo(string panelName)
+    {
+        CloseAllPannel();
+        GameObject panel;
+        if(panels.TryGetValue(panelName, out panel))
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    void CloseAllPannel()
+    {
+        foreach (GameObject panel in panels.Values)
+        {
+            panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public Text nbPlayerText;
+    public Button startButton;
+    // Use this for initialization
+    void Start () {
+        //EventSystemManager.currentSystem.SetSelectedGameObject(theButton);
+        startButton.Select();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        nbPlayerText.text = InputManager.instance.assignedController.Count.ToString();
+    }
+}
